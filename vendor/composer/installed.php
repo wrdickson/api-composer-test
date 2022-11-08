124303bb59ebe455e28ed3c3d3b576e4438cdeb0
Bill Dickson <trekbill@gmail.com>
2022-11-07 17:49:33 -0700

    firebase-added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9aacda03280eb2e68d43b896046eb8635d97183f',
+        'name' => 'wrdickson/apitest',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.3.1',
+            'version' => '6.3.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => 'ddfaddcb520488b42bca3a75e17e9dd53c3667da',
+            'dev_requirement' => false,
+        ),
+        'wrdickson/apitest' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9aacda03280eb2e68d43b896046eb8635d97183f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
