b0600b4277f97e2a2a21794c3066f3ef4ec4b387
Bill Dickson <trekbill@gmail.com>
2022-11-08 15:22:20 -0700

    auth up

--- src/Auth.php
+++ src/Auth.php
@@ -104,14 +104,14 @@ Class Auth {
     $payload = [
       'iat' => $issuedAt->getTimestamp(),  // Issued at: time when the token was generated,
       //  SERVER_NAME is a constant set in config.php
-      'iss' => SERVER_NAME,
+      'iss' => $this->server_name,
       'exp' => $expire,
       'exp_f' => date("Y-m-d H:m:s", $expire),  // Formatted expire
       'nbf'  => $issuedAt->getTimestamp(),  // Not before
       'nbf_f' => date( "Y-m-d H:m:s", $issuedAt->getTimestamp() ),  // Formatted not before
-      'account' => $account->to_array_secure()
+      'account' => $this->account->to_array_secure()
     ];
-    $token = JWT::encode($payload, JWT_KEY, 'HS256');
+    $token = JWT::encode($payload, $this->jwt_key, 'HS256');
     return $token;
   }
 
